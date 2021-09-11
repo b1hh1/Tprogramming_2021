863259fc71d5fab8097144547550625d62e43131
Alexander <71167144+SanyaKirilv@users.noreply.github.com>
2021-09-11 11:36:53 +0300

    Update 11.09
    + First task (a)

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -6,8 +6,20 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine($"Welcome to the club buddy");
+            double a = 2.0f;
+            double b = 3.0f;
+            double y = 0;
+            CalcFunc(a, b, y);
             Console.ReadLine();
+        }
+
+        public static void CalcFunc(double a, double b, double y)
+        {
+            for (float x_start = 0.11f; x_start < 0.36f; x_start += 0.05f)
+            {
+                y = Math.Asin(Math.Pow(x_start, a)) + Math.Acos(Math.Pow(x_start, b));
+                Console.WriteLine("y = " + y);
+            }
         }
     }
 }
