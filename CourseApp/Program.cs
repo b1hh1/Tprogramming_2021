212dd05f645127c63181fc77942e7cb7c5e3d8bd
Alexander <71167144+SanyaKirilv@users.noreply.github.com>
2021-09-11 18:31:55 +0300

    Update 11.09
    +Task B

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,24 +1,40 @@
 ﻿namespace CourseApp
 {
     using System;
+    using static System.Math;
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            double a = 2.0f;
-            double b = 3.0f;
+            double a = 2.0;
+            double b = 3.0;
             double y = 0;
-            CalcFunc(a, b, y);
+            double xStart = 0.11;
+            double xEnd = 0.36;
+            double xDelta = 0.05;
+            double[] xArr = { 0.08, 0.026, 0.35, 0.41, 0.51 };
+            Console.WriteLine($"Task A (a = {a}; b = {b}; xStart = {xStart}; xEnd = {xEnd}; xDelta = {xDelta})");
+            CalculateTaskA(a, b, y, xStart, xEnd, xDelta);
+            Console.WriteLine(" ");
+            Console.WriteLine($"Task B (a = {a}; b = {b}; x1 = {xArr[0]}; x2 = {xArr[1]}; x3 = {xArr[2]}; x4 = {xArr[3]}; x5 = {xArr[4]})");
+            CalculateTaskB(a, b, y, xArr);
             Console.ReadLine();
         }
 
-        public static void CalcFunc(double a, double b, double y)
+        public static void CalculateTaskA(double a, double b, double y, double xStart, double xEnd, double xDelta)
         {
-            for (float x_start = 0.11f; x_start < 0.36f; x_start += 0.05f)
+            for (double x = xStart; x < xEnd; x += xDelta)
             {
-                y = Math.Asin(Math.Pow(x_start, a)) + Math.Acos(Math.Pow(x_start, b));
-                Console.WriteLine("y = " + y);
+                Console.WriteLine($"  y = {Round(Asin(Pow(x, a)) + Acos(Pow(x, b)), 4)}");
+            }
+        }
+
+        public static void CalculateTaskB(double a, double b, double y, double[] xArr)
+        {
+            for (int i = 0; i < xArr.Length; i++)
+            {
+                Console.WriteLine($"  y = {Round(Asin(Pow(xArr[i], a)) + Acos(Pow(xArr[i], b)), 4)}");
             }
         }
     }
