d29941ab3fde93ad77f5edc7883571a357791d12
Alexander <71167144+SanyaKirilv@users.noreply.github.com>
2021-09-14 20:17:39 +0300

    Update 14.09

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -7,31 +7,43 @@
     {
         public static void Main(string[] args)
         {
-            const double a = 2.0;
-            const double b = 3.0;
-            double[] taskOneArray = { 0.11, 0.36, 0.05 };
-            double[] taskTwoArray = { 0.08, 0.026, 0.35, 0.41, 0.51 };
-            Console.WriteLine($"Task A (a = {a}; b = {b}; xStart = {taskOneArray[0]}; xEnd = {taskOneArray[1]}; xDelta = {taskOneArray[2]})");
-            CalculateTaskA(a, b, taskOneArray);
-            Console.WriteLine($"Task B (a = {a}; b = {b}; x1 = {taskTwoArray[0]}; x2 = {taskTwoArray[1]}; x3 = {taskTwoArray[2]}; x4 = {taskTwoArray[3]}; x5 = {taskTwoArray[4]})");
-            CalculateTaskB(a, b, taskTwoArray);
+            var a = 2.0;
+            var b = 3.0;
+            double[][] array = new double[2][];
+            array[0] = new double[] { 0.11, 0.36, 0.05 };
+            array[1] = new double[] { 0.08, 0.026, 0.35, 0.41, 0.51 };
+            Calculate(a, b, array);
             Console.ReadLine();
         }
 
-        public static void CalculateTaskA(double a, double b, double[] taskOneArray)
+        public static void FillArray(double[][] array)
         {
-            for (double x = taskOneArray[0]; x < taskOneArray[1]; x += taskOneArray[2])
+            double[] arr = array[0];
+            var newArrLength = ((arr[1] - arr[0]) / arr[2]) + 1;
+            Array.Resize(ref array[0], (int)newArrLength);
+            for (var i = 0; i < newArrLength; i++)
             {
-                Console.WriteLine($"  y = {Round(Asin(Pow(x, a)) + Acos(Pow(x, b)), 4)}");
+                array[0][i] = arr[0] + (arr[2] * i);
             }
         }
 
-        public static void CalculateTaskB(double a, double b, double[] taskTwoArray)
+        public static void Calculate(double a, double b, double[][] array)
         {
-            for (int i = 0; i < taskTwoArray.Length; i++)
+            FillArray(array);
+            var i = 0;
+            do
             {
-                Console.WriteLine($"  y = {Round(Asin(Pow(taskTwoArray[i], a)) + Acos(Pow(taskTwoArray[i], b)), 4)}");
+                var j = 1;
+                Console.WriteLine($"Task {i + 1}:");
+                foreach (double item in array[i])
+                {
+                    Console.WriteLine($" {j}) x = {item}  y = {Round(Asin(Pow(item, a)) + Acos(Pow(item, b)), 4)}");
+                    j++;
+                }
+
+                i++;
             }
+            while (i < 2);
         }
     }
 }
