0bad2204b0f04947c763f98dfbd9de55d2d2d8c6
Mark_Rumjantsev <mrk.ru@list.ru>
2021-09-25 19:29:06 +0300

    xUnit Theory InlineData

--- CourseApp.Tests/Tests.cs
+++ CourseApp.Tests/Tests.cs
@@ -4,85 +4,15 @@ namespace CourseApp.Tests
 
     public class Tests
     {
-        [Fact]
-        public void TestTaskA1()
+        [Theory]
+        [InlineData(1.23, 1.617)]
+        [InlineData(6.03, 2.996)]
+        [InlineData(3.84, 2.549)]
+        [InlineData(-6.21, 3.39)]
+        public void RunTest(double x, double expected)
         {
             var a = 0.8;
             var b = 0.4;
-            var x = 1.23;
-            var expected = 1.617;
-            var execution = new Calc();
-            var actual = execution.CalcFunc(a, b, x);
-            Assert.Equal(expected, actual, 3);
-        }
-
-        [Fact]
-        public void TestTaskA2()
-        {
-            var a = 0.8;
-            var b = 0.4;
-            var x = 6.03;
-            var expected = 2.996;
-            var execution = new Calc();
-            var actual = execution.CalcFunc(a, b, x);
-            Assert.Equal(expected, actual, 3);
-        }
-
-        [Fact]
-        public void TestTaskA3()
-        {
-            var a = 0.8;
-            var b = 0.4;
-            var x = 3.63;
-            var expected = 2.498;
-            var execution = new Calc();
-            var actual = execution.CalcFunc(a, b, x);
-            Assert.Equal(expected, actual, 3);
-        }
-
-        [Fact]
-        public void TestTaskA4()
-        {
-            var a = 0.8;
-            var b = 0.4;
-            var x = 4.83;
-            var expected = 2.768;
-            var execution = new Calc();
-            var actual = execution.CalcFunc(a, b, x);
-            Assert.Equal(expected, actual, 3);
-        }
-
-        [Fact]
-        public void TestTaskB1()
-        {
-            var a = 0.8;
-            var b = 0.4;
-            var x = 3.84;
-            var expected = 2.549;
-            var execution = new Calc();
-            var actual = execution.CalcFunc(a, b, x);
-            Assert.Equal(expected, actual, 3);
-        }
-
-        [Fact]
-        public void TestTaskB2()
-        {
-            var a = 0.8;
-            var b = 0.4;
-            var x = 4.55;
-            var expected = 2.709;
-            var execution = new Calc();
-            var actual = execution.CalcFunc(a, b, x);
-            Assert.Equal(expected, actual, 3);
-        }
-
-        [Fact]
-        public void TestTaskB3()
-        {
-            var a = 0.8;
-            var b = 0.4;
-            var x = -6.21;
-            var expected = 3.39;
             var execution = new Calc();
             var actual = execution.CalcFunc(a, b, x);
             Assert.Equal(expected, actual, 3);
