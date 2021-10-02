f2699d73c441ac1764d9757613c743a193166d6c
Ihate Shestakov <baranka2003@yandex.ru>
2021-10-02 15:18:20 +0300

    Function with tests

---
+++ CourseApp.Tests/FunctionTest.cs
@@ -0,0 +1,63 @@
+﻿namespace CourseApp.Tests
+{
+    using Xunit;
+
+    public class FunctionTest
+    {
+        [Fact]
+        public void FirstTest()
+        {
+            var calc = new FunctionCalculator();
+            double a = 4.1;
+            double b = 2.7;
+            double x = 2.34;
+            var exp = 1956.21338;
+
+            var res = calc.Function(a, b, x);
+
+            Assert.Equal(exp, res, 5);
+        }
+
+        [Fact]
+        public void XEqualOneTest()
+        {
+            var calc = new FunctionCalculator();
+            double a = 4.1;
+            double b = 2.7;
+            double x = 1;
+            var exp = 0;
+
+            var res = calc.Function(a, b, x);
+
+            Assert.Equal(exp, res);
+        }
+
+        [Fact]
+        public void AEqual0Test()
+        {
+            var calc = new FunctionCalculator();
+            double a = 0;
+            double b = 2.7;
+            double x = 1.9;
+            var exp = 11239.29616;
+
+            var res = calc.Function(a, b, x);
+
+            Assert.Equal(exp, res, 5);
+        }
+
+        [Fact]
+        public void BEqual0Test()
+        {
+            var calc = new FunctionCalculator();
+            double a = 4.1;
+            double b = 0;
+            double x = 1.9;
+            var exp = -53004.79111;
+
+            var res = calc.Function(a, b, x);
+
+            Assert.Equal(exp, res, 4);
+        }
+    }
+}
---
+++ CourseApp/FunctionCalculator.cs
@@ -0,0 +1,32 @@
+﻿namespace CourseApp
+{
+    using System;
+
+    public class FunctionCalculator
+    {
+        public double Function(double a, double b, double x)
+        {
+            double num = (a * Math.Pow(x, 1.0 / 3.0)) - (b * Math.Log10(x) / Math.Log10(5.0));
+            double denum = Math.Pow(Math.Log10(x - 1), 3.0);
+            double y = num / denum;
+            Console.WriteLine(y);
+            return y;
+        }
+
+        public void TaskA(double xStart, double xEnd, double dX, double a, double b)
+        {
+            for (double x = xStart; x < xEnd; x += dX)
+            {
+                Function(a, b, x);
+            }
+        }
+
+        public void TaskB(double[] nums, double a, double b)
+        {
+            foreach (double num in nums)
+            {
+                Function(a, b, num);
+            }
+        }
+    }
+}
