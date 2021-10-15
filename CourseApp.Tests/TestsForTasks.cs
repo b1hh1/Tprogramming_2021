edc433822dd1d534916384a9b2325065467a622c
Alexander <71167144+SanyaKirilv@users.noreply.github.com>
2021-10-15 16:02:38 +0300

    Change tests

--- CourseApp.Tests/TestsForTasks.cs
+++ CourseApp.Tests/TestsForTasks.cs
@@ -1,7 +1,6 @@
 namespace CourseApp.Tests
 {
     using Xunit;
-    using static System.Math;
 
     public class TestsForTasks
     {
@@ -53,6 +52,34 @@ namespace CourseApp.Tests
         {
             var check = new CheckValues();
             var actual = check.IsValidDouble(input, min, max);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("2", 0, 3, true)]
+        [InlineData("-1", 0, 3, false)]
+        [InlineData("r", 0, 3, false)]
+        [InlineData("-1.2", -2, -1, false)]
+        [InlineData("4.1", 4, 5, false)]
+        [InlineData(" ", 4, 4, false)]
+        public void TestForInputInt(string item, int minValue, int maxValue, bool expected)
+        {
+            var input = new InputValues();
+            var actual = input.InputInt(minValue, maxValue, item);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("2,2", 0, 3, true)]
+        [InlineData("-1,5", 0, 3, false)]
+        [InlineData("r", 0, 3, false)]
+        [InlineData("-1", -2, -1, true)]
+        [InlineData("4,1", 4, 5, true)]
+        [InlineData(" ", 4, 4, false)]
+        public void TestForInputDouble(string item, int minValue, int maxValue, bool expected)
+        {
+            var input = new InputValues();
+            var actual = input.InputDouble(minValue, maxValue, item);
             Assert.Equal(expected, actual);
         }
     }
--- CourseApp/InputValues.cs
+++ CourseApp/InputValues.cs
@@ -44,6 +44,19 @@ namespace CourseApp
             return intInput;
         }
 
+        public bool InputInt(int minValue, int maxValue, string item)
+        {
+            int intInput = 0;
+            var isCorrect = false;
+            var check = new CheckValues();
+            if (int.TryParse(item, out intInput))
+            {
+                isCorrect = check.IsValidInt(intInput, minValue, maxValue);
+            }
+
+            return isCorrect;
+        }
+
         public double InputDouble(double maxValue)
         {
             return InputDouble(0, maxValue);
@@ -68,6 +81,19 @@ namespace CourseApp
             }
 
             return doubleInput;
+        }
+
+        public bool InputDouble(double minValue, double maxValue, string item)
+        {
+            double doubleInput = 0;
+            var isCorrect = false;
+            var check = new CheckValues();
+            if (double.TryParse(item, out doubleInput))
+            {
+                isCorrect = check.IsValidDouble(doubleInput, minValue, maxValue);
+            }
+
+            return isCorrect;
         }
     }
 }
